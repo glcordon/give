06c2a59df0a55b40366e157725877933a1fc581c
Gary Cordon - ST <gary.cordon@servicetrade.com>
2025-03-05 00:07:46 -0500

    Fixed some styling

--- resources/views/livewire/public-event-proposal-form.blade.php
+++ resources/views/livewire/public-event-proposal-form.blade.php
@@ -1,8 +1,13 @@
-<div>
+<div style="background-color: oklch(0.21 0.034 264.665);">
     <form wire:submit.prevent="submit">
         @foreach ($this->form->getComponents() as $component)
         {!! $component->render() !!}
         @endforeach
-        <button type="submit" class="btn btn-primary">Submit Proposal</button>
+        <button type="submit" class="btn btn-primary cursor-pointer" style="margin:5px 10px; border-radius:10px; color:white; padding:20px 10px; background-color:blue;">Submit Proposal</button>
     </form>
+    <style>
+        .fi-fo-field-wrp-error-message {
+            color: red;
+        }
+    </style>
 </div>
